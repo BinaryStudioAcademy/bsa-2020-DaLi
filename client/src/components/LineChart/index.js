cdc098bc60175377df2ee96031f3a6b7bd725a8c
ZhannaMelnyk <58272440+ZhannaMelnyk@users.noreply.github.com>
2020-08-04 13:30:39 +0300

    Create LineChartComponent and LineChartContainer

---
+++ client/src/components/LineChart/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LineChart = () => {
+  return <div>
+
+  </div>
+};
+
+export default LineChart;
---
+++ client/src/containers/LineChartContainer/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import LineChart from '../../components/LineChart';
+
+class LineChartContainer extends Component {
+  render() {
+    return <LineChart />
+  }
+};
+
+export default LineChartContainer;

df48705a52d9bced795cd4b16606d64bca44fcd6
Ivan Patsai <ivan.patsai@binary-studio.com>
2020-08-04 08:27:24 +0300

    split fe and be into folders

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 console.log(process.env.DATABASE_URL);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+  dialect: 'postgres',
 });
 
 const models = {};

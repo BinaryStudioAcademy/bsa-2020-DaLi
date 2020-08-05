0473793b146846ffa04153947ad932654ef31337
Natali <4natalionly@gmail.com>
2020-08-05 11:24:34 +0300

    Added materialUI Icons

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ function App() {
                 </Switch>
             </Router>
 
-
             {/*<Typography variant='h6' className="App-header">*/}
             {/*  <img src={logo} className="App-logo" alt="logo" />*/}
             {/*  <p>*/}
--- client/src/components/SelectVisualization/SelectVisualization.js
+++ client/src/components/SelectVisualization/SelectVisualization.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {useHistory} from 'react-router-dom'
+import ShowChartSharpIcon from '@material-ui/icons/ShowChartSharp';
+import BarChartOutlinedIcon from '@material-ui/icons/BarChartOutlined';
+import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
 import './styles.css'
 
 const SelectVisualization = () => {
@@ -10,19 +13,19 @@ const SelectVisualization = () => {
                 <ul className="visualization-list">
                     <li className="visualization-item">
                         <div className="visualization-icon" onClick={() => history.push('/createChar')}>
-
+                            <ShowChartSharpIcon color="primary" style={{fontSize: 100}}/>
                         </div>
                         <p>Line chart</p>
                     </li>
                     <li className="visualization-item">
                         <div className="visualization-icon" onClick={() => history.push('/createChar')}>
-
+                            <BarChartOutlinedIcon color="primary" style={{fontSize: 100}}/>
                         </div>
                         <p>Bar chart</p>
                     </li>
                     <li className="visualization-item">
                         <div className="visualization-icon" onClick={() => history.push('/createChar')}>
-
+                            <MapOutlinedIcon color="primary" variant="filled" style={{fontSize: 100}}/>
                         </div>
                         <p>Map</p>
                     </li>

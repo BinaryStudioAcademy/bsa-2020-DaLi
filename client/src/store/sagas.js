7241591ab8c8ef9e87ab13ea2a9eaf17fc901e1f
Makc <woohoo1607@gmail.com>
2020-08-07 14:48:39 +0300

    feat: select visualization component added

--- client/src/store/sagas.js
+++ client/src/store/sagas.js
@@ -1,6 +1,5 @@
-import { all } from "redux-saga/effects";
-import  loginSagas  from "../containers/LoginPageContainer/sagas";
-
+import { all } from 'redux-saga/effects';
+import loginSagas from '../containers/LoginPageContainer/sagas';
 
 export function* rootSaga() {
   yield all([loginSagas()]);

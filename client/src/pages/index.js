f8d6d25b56815f33431a4ff568d7f50a711733a9
Serhii Taran <serjtaran@gmail.com>
2020-08-09 21:54:41 +0300

    feat: add account settings page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,5 @@
 import LoginPage from './LoginPage';
 import SelectVisualizationPage from './SelectVisualizationPage/SelectVisualizationPage';
+import AccountSettingsPage from './AccountSettingsPage';
 
-export { LoginPage, SelectVisualizationPage };
+export { LoginPage, SelectVisualizationPage, AccountSettingsPage };

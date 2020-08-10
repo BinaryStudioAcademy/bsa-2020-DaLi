cc892c39a7685bf9da6c7f3f4f9a4319ffb35d26
tarkus2000ua <tarkus@i.ua>
2020-08-10 15:50:47 +0300

    CRUD visualizations added

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,14 +2,15 @@ import Sequelize from 'sequelize';
 import DATABASE_URL from '../config/dbConfig';
 import User from './user';
 import VisualizationConfigModel from './visualizationConfig';
+import Visualization from './visualization';
 
 export const sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
 });
 
 User(sequelize, Sequelize.DataTypes);
+Visualization(sequelize, Sequelize.DataTypes, Sequelize.Deferrable);
 VisualizationConfigModel(sequelize, Sequelize.DataTypes);
-User(sequelize, Sequelize.DataTypes);
 
 const models = sequelize.models;
 

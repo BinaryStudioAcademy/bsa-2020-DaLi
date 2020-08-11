609029c4dc1a14ff7bee67718034d0d14b710953
Serhii Taran <serjtaran@gmail.com>
2020-08-11 21:40:07 +0300

    feat: add ability to set type column in init config  of table visualization

--- client/src/containers/ViewVisualizationContainer/helpers/initConfigsHelper/tableConfig.js
+++ client/src/containers/ViewVisualizationContainer/helpers/initConfigsHelper/tableConfig.js
@@ -1,23 +1,65 @@
-function createInitTableConfig(dataSample) {
-  const columns = [];
-  const createColumn = (id, order) => {
-    return {
-      id,
-      title: id.charAt(0).toUpperCase() + id.substr(1),
-      order,
-    };
-  };
+const checkIsId = (key) => key.toLowerCase().search('id') !== -1;
 
-  Object.keys(dataSample).forEach((key, index) => {
-    columns.push(createColumn(key, index));
-  });
-  return {
-    columns,
-    sort: {
-      order: 'asc',
-      orderBy: 'id',
-    },
+const checkIsNumber = (value) => {
+  return typeof value === 'number';
+};
+const checkIsDate = (value) => new Date(Number(value)).toDateString() !== 'Invalid Date';
+
+const setColumnType = (key, value) => {
+  // if value no exist, i expect number type
+  if (!value.toString().length) {
+    return 'number';
+  }
+  const isId = checkIsId(key);
+  if (isId) {
+    return 'id';
+  }
+  const isNumber = checkIsNumber(value);
+  if (isNumber) {
+    return 'number';
+  }
+  const isDate = checkIsDate(value);
+  if (isDate) {
+    return 'date';
+  }
+  return 'string';
+};
+
+const createColumnTitle = (key) => key.charAt(0).toUpperCase() + key.substr(1);
+
+const createTableColumn = (key, index, value) => {
+  const column = {
+    id: key,
+    title: createColumnTitle(key),
+    order: index,
+    type: setColumnType(key, value),
   };
+  return column;
+};
+
+const createTableColumns = (dataSample) => {
+  const columns = [];
+  Object.keys(dataSample).forEach((key, index) => {
+    const value = dataSample[key];
+    const column = createTableColumn(key, index, value);
+    columns.push(column);
+  });
+  return columns;
+};
+
+const initTableSort = {
+  order: 'asc',
+  orderBy: 'id',
+};
+
+function createInitTableConfig(dataSample) {
+  const columns = createTableColumns(dataSample);
+  const sort = initTableSort;
+  const initTableConfig = {
+    columns,
+    sort,
+  };
+  return initTableConfig;
 }
 
 export default createInitTableConfig;

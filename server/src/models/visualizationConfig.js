f8769e09553389228006731aef9707a354e7c4b0
tarkus2000ua <tarkus@i.ua>
2020-08-11 14:50:10 +0300

    removed routes, service and repository for viualisation config

--- server/src/models/visualizationConfig.js
+++ server/src/models/visualizationConfig.js
@@ -1,21 +1,17 @@
 const visualizationConfig = (sequelize, DataTypes) => {
-  const VisualizationConfig = sequelize.define(
-    'visualizationConfig',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      color: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE,
+  const VisualizationConfig = sequelize.define('visualizationConfig', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {},
-  );
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  });
 
   return VisualizationConfig;
 };

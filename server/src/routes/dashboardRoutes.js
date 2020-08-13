094be02363ee7702128ec279574c7aed578df432
Maksym <woohoo1607@gmail.com>
2020-08-13 11:46:32 +0300

    fix: console.log deleted

--- server/src/routes/dashboardRoutes.js
+++ server/src/routes/dashboardRoutes.js
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
   const result = await DashboardService.getDashboards();
-  console.log(result);
   res.status(200).json({ error: false, result });
   next();
 });

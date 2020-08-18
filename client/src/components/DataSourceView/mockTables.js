a2d3706ba9d8dd33441976dfd1accba00877d99b
ZhannaMelnyk <58272440+ZhannaMelnyk@users.noreply.github.com>
2020-08-18 16:36:06 +0300

    add styles and mock tables

---
+++ client/src/components/DataSourceView/mockTables.js
@@ -0,0 +1,22 @@
+export const mockTables = [
+  {
+    id: '1',
+    name: 'Orders',
+    description: 'this is Orders',
+  },
+  {
+    id: '2',
+    name: 'People',
+    description: 'this is People',
+  },
+  {
+    id: '3',
+    name: 'Products',
+    description: 'this is Products',
+  },
+  {
+    id: '3',
+    name: 'Reviews',
+    description: 'this is Reviews',
+  },
+];

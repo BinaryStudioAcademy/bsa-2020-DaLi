4c9b6e7c7499f0f4061dc56e096b853a605502ce
Nikita Remeslov <creatorbon@gmail.com>
2020-08-19 11:58:33 +0300

    Add database link navigation

--- client/src/components/DatabaseList/DatabaseListItem.js
+++ client/src/components/DatabaseList/DatabaseListItem.js
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import { useStyles } from './styles';
 
-const DatabaseListItem = ({ database: { id, name, type }, onDelete }) => {
+const DatabaseListItem = ({ database: { id, dbName, type }, onDelete }) => {
   const classes = useStyles();
 
   return (
     <>
       <TableRow className={classes.tableRow}>
         <TableCell align="left" className={classes.databaseName}>
-          {name}
+          <Link
+            className={classes.item}
+            to={{
+              pathname: `/admin/databases/${id}`,
+            }}
+          >
+            {dbName}
+          </Link>
         </TableCell>
         <TableCell align="left">{type}</TableCell>
         <TableCell align="right">

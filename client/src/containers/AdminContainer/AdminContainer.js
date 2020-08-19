c660f39f97ef449ca8759c4a5eda60ce65d05e07
Nikita Remeslov <creatorbon@gmail.com>
2020-08-19 12:28:08 +0300

    Add databases/:id route

--- client/src/containers/AdminContainer/AdminContainer.js
+++ client/src/containers/AdminContainer/AdminContainer.js
@@ -9,6 +9,13 @@ function AdminContainer() {
       <Switch>
         <Route path="/admin/people" component={PeoplePageContainer} />
         <Route exact path="/admin/databases" component={DatabasesPageContainer} />
+        <Route
+          exact
+          path="/admin/databases/:id"
+          component={({ match }) => {
+            return <div>Database id {match.params.id}</div>;
+          }}
+        />
         <Route exact path="/admin/permissions" component={() => <div>permissions</div>} />
       </Switch>
     </main>

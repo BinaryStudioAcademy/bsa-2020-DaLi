55533ac331fce2420bea0f7e3f609fc5c84e8789
Makc <woohoo1607@gmail.com>
2020-08-20 10:45:46 +0300

    feat: added userCount to UserGroupsPage

--- client/src/components/GroupList/RowItem.js
+++ client/src/components/GroupList/RowItem.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import Avatar from "@material-ui/core/Avatar";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import {useStyles} from "./styles";
-import UserGroupForm from "./UserGroupForm";
+import Avatar from '@material-ui/core/Avatar';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import PropTypes from 'prop-types';
+import { useStyles } from './styles';
+import UserGroupForm from './UserGroupForm';
 
-const RowItem = ({group, deleteGroup, updateUserGroup}) => {
+const RowItem = ({ group, deleteGroup, updateUserGroup }) => {
   const classes = useStyles();
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const [isActiveEditMode, setIsActiveEditMode] = useState(false);
@@ -19,7 +19,7 @@ const RowItem = ({group, deleteGroup, updateUserGroup}) => {
   };
 
   const changeActiveEditMode = () => {
-    setIsActiveEditMode(!isActiveEditMode)
+    setIsActiveEditMode(!isActiveEditMode);
   };
 
   const closeForm = (resetForm) => () => {
@@ -31,36 +31,40 @@ const RowItem = ({group, deleteGroup, updateUserGroup}) => {
   const saveChange = (values) => {
     const payload = {
       id: group.id,
-      data: {...values}
+      data: { ...values },
     };
     updateUserGroup(payload);
     setMenuAnchorEl(null);
-  }
+  };
 
   return (
-      <>
-        {!isActiveEditMode && <TableRow key={group.name}>
+    <>
+      {!isActiveEditMode && (
+        <TableRow key={group.name}>
           <TableCell align="left" className={classes.name}>
             <Avatar className={classes.avatar}>{group.name[0]}</Avatar>
             {group.name}
           </TableCell>
-          <TableCell align="left">{/* {group.members} */}???</TableCell>
+          <TableCell align="left">{group.userCount}</TableCell>
           <TableCell align="left">
-            <MoreHorizIcon className={classes.dots} onClick={handleMenuClick}/>
+            <MoreHorizIcon className={classes.dots} onClick={handleMenuClick} />
             <Menu
-                id="add-menu"
-                anchorEl={menuAnchorEl}
-                keepMounted
-                open={Boolean(menuAnchorEl)}
-                onClose={() => setMenuAnchorEl(null)}
+              id="add-menu"
+              anchorEl={menuAnchorEl}
+              keepMounted
+              open={Boolean(menuAnchorEl)}
+              onClose={() => setMenuAnchorEl(null)}
             >
               <MenuItem onClick={changeActiveEditMode}>Edit name</MenuItem>
               <MenuItem onClick={deleteGroup(group.id)}>Remove group</MenuItem>
             </Menu>
           </TableCell>
-        </TableRow>}
-        {isActiveEditMode && <UserGroupForm submitTitle='Edit' initialName={group.name} submit={saveChange} closeForm={closeForm}/>}
-      </>
+        </TableRow>
+      )}
+      {isActiveEditMode && (
+        <UserGroupForm submitTitle="Edit" initialName={group.name} submit={saveChange} closeForm={closeForm} />
+      )}
+    </>
   );
 };
 
@@ -68,6 +72,6 @@ RowItem.propTypes = {
   group: PropTypes.array,
   deleteGroup: PropTypes.func,
   updateUserGroup: PropTypes.func,
-}
+};
 
 export default RowItem;

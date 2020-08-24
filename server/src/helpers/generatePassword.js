9237fc63f333e40d6ef80717cc787a7fda1c9e31
Nikita Remeslov <creatorbon@gmail.com>
2020-08-25 01:06:44 +0300

    Update generatePassword function

--- server/src/helpers/generatePassword.js
+++ server/src/helpers/generatePassword.js
@@ -1,18 +1,32 @@
-export function generatePassword(passwordLength = 8, useUpperCase = true, useNumbers = true, useSpecialChars = true) {
+/* eslint-disable */
+export function generatePassword(passwordLength = 8) {
   const chars = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCaseChars = chars.toUpperCase();
   const numberChars = '0123456789';
-  const specialChars = '!£$%^&*()';
+  const specialChars = '!@#$%^&*()_+:<>?|;,.~';
 
-  const usableChars =
-    chars +
-    (useUpperCase ? chars.toUpperCase() : '') +
-    (useNumbers ? numberChars : '') +
-    (useSpecialChars ? specialChars : '');
+  const specialCharsReg = /[-!$%^&*()_+|~=:;<>?,#@.]/g;
+  const numberCharsReg = /[0-9]+/g;
+  const upperCaseReg = /[A-Z]/g;
+
+  const getRandomIndex = (limit) => Math.floor(Math.random() * limit);
+
+  const usableChars = chars + upperCaseChars + numberChars + specialChars;
 
   let generatedPassword = '';
 
   for (let i = 0; i <= passwordLength; i++) {
-    generatedPassword += usableChars[Math.floor(Math.random() * usableChars.length)];
+    generatedPassword += usableChars[getRandomIndex(usableChars.length)];
+  }
+
+  if (!generatedPassword.match(specialCharsReg)) {
+    generatedPassword += specialChars[getRandomIndex(specialChars.length)];
+  }
+  if (!generatedPassword.match(numberCharsReg)) {
+    generatedPassword += numberChars[getRandomIndex(numberChars.length)];
+  }
+  if (!generatedPassword.match(upperCaseReg)) {
+    generatedPassword += upperCaseChars[getRandomIndex(upperCaseChars.length)];
   }
 
   return generatedPassword;

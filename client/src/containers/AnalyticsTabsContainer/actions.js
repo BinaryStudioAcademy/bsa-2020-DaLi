53e01fe45c80f60cd3ff31672e95e0b34e688060
Nikita Remeslov <creatorbon@gmail.com>
2020-09-04 02:04:19 +0300

    Add Collection redux actions

--- client/src/containers/AnalyticsTabsContainer/actions.js
+++ client/src/containers/AnalyticsTabsContainer/actions.js
@@ -1,8 +1,22 @@
-import { GET_ANALYTICS, DELETE_VISUALIZATION, ADD_DASHBOARD, DELETE_DASHBOARD } from './actionsTypes';
+import {
+  GET_COLLECTIONS,
+  DELETE_VISUALIZATION,
+  ADD_DASHBOARD,
+  DELETE_DASHBOARD,
+  OPEN_MODAL,
+  CLOSE_MODAL,
+  SET_FORM_DATA,
+  ADD_COLLECTION,
+  DELETE_COLLECTION,
+  UPDATE_COLLECTION,
+  RESET_NOTIFICATION,
+  GET_CURRENT_COLLECTION,
+  MOVE_TO_COLLECTION,
+} from './actionsTypes';
 
 export const getAnalytics = () => {
   return {
-    type: GET_ANALYTICS,
+    type: GET_COLLECTIONS,
   };
 };
 
@@ -20,9 +34,71 @@ export const addDashboard = (newDashboard) => {
   };
 };
 
-export const deleteDashboard = (id) => {
+export const deleteDashboard = ({ id, collectionId }) => {
   return {
     type: DELETE_DASHBOARD,
     id,
+    collectionId,
+  };
+};
+
+export const addCollection = (payload) => {
+  return {
+    type: ADD_COLLECTION,
+    payload,
+  };
+};
+
+export const deleteCollection = (id) => {
+  return {
+    type: DELETE_COLLECTION,
+    id,
+  };
+};
+
+export const updateCollection = (payload) => {
+  return {
+    type: UPDATE_COLLECTION,
+    payload,
+  };
+};
+
+export const openModal = (payload = {}) => {
+  return {
+    type: OPEN_MODAL,
+    payload,
+  };
+};
+
+export const closeModal = () => {
+  return {
+    type: CLOSE_MODAL,
+  };
+};
+
+export const setFormData = (payload) => {
+  return {
+    type: SET_FORM_DATA,
+    payload,
+  };
+};
+
+export const resetNotification = () => {
+  return {
+    type: RESET_NOTIFICATION,
+  };
+};
+
+export const getCollection = (id) => {
+  return {
+    type: GET_CURRENT_COLLECTION,
+    id,
+  };
+};
+
+export const moveToCollection = (payload) => {
+  return {
+    type: MOVE_TO_COLLECTION,
+    payload,
   };
 };
--- client/src/containers/AnalyticsTabsContainer/sagas.js
+++ client/src/containers/AnalyticsTabsContainer/sagas.js
@@ -1,30 +1,55 @@
 import { put, call, takeEvery, all } from 'redux-saga/effects';
 import {
-  GET_ANALYTICS,
-  GET_ANALYTICS_SUCCESS,
-  GET_ANALYTICS_ERROR,
+  GET_COLLECTIONS,
+  GET_COLLECTIONS_SUCCESS,
+  GET_COLLECTIONS_ERROR,
   DELETE_VISUALIZATION,
   ADD_DASHBOARD,
   DELETE_DASHBOARD,
   FETCH_VISUALIZATIONS,
   FETCH_VISUALIZATIONS_SUCCESS,
   FETCH_VISUALIZATIONS_ERROR,
+  ADD_COLLECTION,
+  DELETE_COLLECTION,
+  UPDATE_COLLECTION,
+  CLOSE_MODAL,
+  ADD_COLLECTION_SUCCESS,
+  ADD_COLLECTION_ERROR,
+  GET_CURRENT_COLLECTION,
+  GET_CURRENT_COLLECTION_ERROR,
+  GET_CURRENT_COLLECTION_SUCCESS,
+  MOVE_TO_COLLECTION,
+  MOVE_TO_COLLECTION_SUCCESS,
+  MOVE_TO_COLLECTION_ERROR,
+  DELETE_COLLECTION_SUCCESS,
+  DELETE_COLLECTION_ERROR,
+  UPDATE_COLLECTION_SUCCESS,
+  UPDATE_COLLECTION_ERROR,
 } from './actionsTypes';
 import { visualizationsAPIService } from '../../services/api/visualizationsAPI.service';
 import { dashboardsAPIService } from '../../services/api/dashboardsAPI.service';
+import { collectionsAPIService } from '../../services/api/collectionsAPI.service';
 
 export function* getAnalytics() {
   try {
-    const visualizations = yield call(visualizationsAPIService.getVisualizations);
-    const dashboards = yield call(dashboardsAPIService.getDashboards);
-    yield put({ type: GET_ANALYTICS_SUCCESS, payload: { visualizations, dashboards } });
+    const { collections, defaultCollection } = yield call(collectionsAPIService.getCollections);
+    const { dashboards, visualizations, ...collection } = defaultCollection;
+    yield put({
+      type: GET_COLLECTIONS_SUCCESS,
+      payload: {
+        visualizations,
+        dashboards,
+        collections,
+        currentCollection: collection,
+      },
+    });
   } catch (error) {
-    yield put({ type: GET_ANALYTICS_ERROR, payload: error });
+    yield put({ type: GET_COLLECTIONS_ERROR, payload: error });
   }
 }
 
 export function* watchGetAnalyticsSaga() {
-  yield takeEvery(GET_ANALYTICS, getAnalytics);
+  yield takeEvery(GET_COLLECTIONS, getAnalytics);
 }
 
 export function* fetchVisualizationsSaga() {
@@ -42,7 +67,7 @@ export function* watchFetchVisualizationsSaga() {
 
 export function* deleteVisualizationSaga(payload) {
   yield call(visualizationsAPIService.deleteVisualization, payload.id);
-  yield put({ type: GET_ANALYTICS });
+  yield put({ type: GET_COLLECTIONS });
 }
 
 export function* watchDeleteVisualizationSaga() {
@@ -51,7 +76,7 @@ export function* watchDeleteVisualizationSaga() {
 
 export function* addDashboardSaga(payload) {
   yield call(dashboardsAPIService.createDashboard, payload.newDashboard);
-  yield put({ type: GET_ANALYTICS });
+  yield put({ type: GET_COLLECTIONS });
 }
 
 export function* watchAddDashboardSaga() {
@@ -60,11 +85,107 @@ export function* watchAddDashboardSaga() {
 
 export function* deleteDashboardSaga(payload) {
   yield call(dashboardsAPIService.deleteDashboard, payload.id);
-  yield put({ type: GET_ANALYTICS });
+  if (payload.collectionId) {
+    yield put({ type: GET_CURRENT_COLLECTION, id: payload.collectionId });
+  } else {
+    yield put({ type: GET_COLLECTIONS });
+  }
 }
 
 export function* watchDeleteDashboardSaga() {
   yield takeEvery(DELETE_DASHBOARD, deleteDashboardSaga);
+}
+
+export function* addCollectionSaga({ payload }) {
+  try {
+    yield call(collectionsAPIService.addCollection, payload);
+    yield put({ type: ADD_COLLECTION_SUCCESS });
+    yield put({ type: CLOSE_MODAL });
+    yield put({ type: GET_COLLECTIONS });
+  } catch (error) {
+    yield put({ type: ADD_COLLECTION_ERROR, payload: error });
+  }
+}
+
+export function* watchAddCollectionSaga() {
+  yield takeEvery(ADD_COLLECTION, addCollectionSaga);
+}
+
+export function* moveToCollectionSaga({ payload }) {
+  try {
+    let result;
+    if (payload.dashboardId) {
+      result = yield call(dashboardsAPIService.addToCollection, payload.dashboardId, payload.collection);
+    } else {
+      result = yield call(visualizationsAPIService.addToCollection, payload.visualizationId, payload.collection);
+    }
+
+    yield put({ type: MOVE_TO_COLLECTION_SUCCESS, payload: result.status });
+    yield put({ type: CLOSE_MODAL });
+
+    if (payload.collectionId) {
+      yield put({ type: GET_CURRENT_COLLECTION, id: payload.collectionId });
+    } else {
+      yield put({ type: GET_COLLECTIONS });
+    }
+  } catch (error) {
+    yield put({ type: MOVE_TO_COLLECTION_ERROR, payload: error });
+  }
+}
+
+export function* watchMoveToCollectionSaga() {
+  yield takeEvery(MOVE_TO_COLLECTION, moveToCollectionSaga);
+}
+
+export function* deleteCollectionSaga({ id }) {
+  try {
+    yield call(collectionsAPIService.deleteCollection, id);
+    yield put({ type: DELETE_COLLECTION_SUCCESS });
+    yield put({ type: GET_COLLECTIONS });
+  } catch (error) {
+    yield put({ type: DELETE_COLLECTION_ERROR, payload: error });
+  }
+}
+
+export function* watchDeleteCollectionSaga() {
+  yield takeEvery(DELETE_COLLECTION, deleteCollectionSaga);
+}
+
+export function* updateCollectionSaga({ payload }) {
+  try {
+    yield call(collectionsAPIService.updateCollection, payload.id, payload.data);
+    yield put({ type: CLOSE_MODAL });
+    yield put({ type: UPDATE_COLLECTION_SUCCESS });
+    yield put({ type: GET_CURRENT_COLLECTION, id: payload.id });
+  } catch (error) {
+    yield put({ type: UPDATE_COLLECTION_ERROR, payload: error });
+  }
+}
+
+export function* watchUpdateCollectionSaga() {
+  yield takeEvery(UPDATE_COLLECTION, updateCollectionSaga);
+}
+
+export function* getCollectionSaga(payload) {
+  try {
+    const { collections } = yield call(collectionsAPIService.getCollections);
+    const { dashboards, visualizations, ...collection } = yield call(collectionsAPIService.getCollection, payload.id);
+    yield put({
+      type: GET_CURRENT_COLLECTION_SUCCESS,
+      payload: {
+        visualizations,
+        dashboards,
+        currentCollection: collection,
+        collections,
+      },
+    });
+  } catch (error) {
+    yield put({ type: GET_CURRENT_COLLECTION_ERROR, payload: error });
+  }
+}
+
+export function* watchGetCollectionSaga() {
+  yield takeEvery(GET_CURRENT_COLLECTION, getCollectionSaga);
 }
 
 export default function* analyticsSaga() {
@@ -74,5 +195,10 @@ export default function* analyticsSaga() {
     watchAddDashboardSaga(),
     watchDeleteDashboardSaga(),
     watchFetchVisualizationsSaga(),
+    watchAddCollectionSaga(),
+    watchDeleteCollectionSaga(),
+    watchUpdateCollectionSaga(),
+    watchGetCollectionSaga(),
+    watchMoveToCollectionSaga(),
   ]);
 }

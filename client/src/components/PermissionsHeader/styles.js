82d2ace534f4fd5a3f1ddd518c47a86d01b56407
ZhannaMelnyk <58272440+ZhannaMelnyk@users.noreply.github.com>
2020-09-08 02:01:44 +0300

    add default styles for permissions

--- client/src/components/PermissionsHeader/styles.js
+++ client/src/components/PermissionsHeader/styles.js
@@ -1,44 +1,44 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
-  // permissionsEditingHeader: {
-  //   display: 'flex',
-  //   height: '60px',
-  //   alignItems: 'center',
-  //   borderBottom: '1px solid #f3f4f4',
-  //   backgroundColor: '#8588b6',
-  //   padding: '0 40px',
-  //   boxSizing: 'border-box',
-  //   justifyContent: 'space-between',
-  // },
-  // permissionsEditingHeaderTitle: {
-  //   fontSize: '15px',
-  //   color: '#f6f6f9',
-  // },
-  // permissionsEditingHeaderButtons: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   justifyContent: 'space-between',
-  // },
-  // permissionsEditingHeaderButton: {
-  //   padding: '0px 15px',
-  //   fontSize: '14px',
-  //   boxSizing: 'border-box',
-  //   height: '35px',
-  //   textTransform: 'none',
-  //   marginLeft: '20px',
-  // },
-  // permissionsEditingHeaderButtonCancel: {
-  //   color: '#faf7fa',
-  //   background: '#9294bd',
-  // },
-  // permissionsEditingHeaderButtonSave: {
-  //   color: '#4e4f50',
-  //   background: '#ffffff',
-  //   '&:hover': {
-  //     background: '#eee',
-  //   },
-  // },
+  permissionsEditingHeader: {
+    display: 'flex',
+    height: '60px',
+    alignItems: 'center',
+    borderBottom: '1px solid #f3f4f4',
+    backgroundColor: '#8588b6',
+    padding: '0 40px',
+    boxSizing: 'border-box',
+    justifyContent: 'space-between',
+  },
+  permissionsEditingHeaderTitle: {
+    fontSize: '15px',
+    color: '#f6f6f9',
+  },
+  permissionsEditingHeaderButtons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  permissionsEditingHeaderButton: {
+    padding: '0px 15px',
+    fontSize: '14px',
+    boxSizing: 'border-box',
+    height: '35px',
+    textTransform: 'none',
+    marginLeft: '20px',
+  },
+  permissionsEditingHeaderButtonCancel: {
+    color: '#faf7fa',
+    background: '#9294bd',
+  },
+  permissionsEditingHeaderButtonSave: {
+    color: '#4e4f50',
+    background: '#ffffff',
+    '&:hover': {
+      background: '#eee',
+    },
+  },
 }));
 
 export default useStyles;
--- client/src/components/PermissionsTable/styles.js
+++ client/src/components/PermissionsTable/styles.js
@@ -1,34 +1,34 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
-  // permissionsTableBreadcrumbs: {
-  //   padding: '0 50px',
-  //   paddingTop: '60px',
-  // },
-  // permissionsTableLink: {
-  //   color: '#8cb7e6',
-  //   textDecoration: 'none',
-  // },
-  // permissionsTableLinkActive: {
-  //   color: '#242424',
-  //   cursor: 'default',
-  //   textDecoration: 'none',
-  // },
-  // permissionsTableContainer: {
-  //   padding: '0 40px',
-  //   paddingBottom: '60px',
-  //   boxSizing: 'border-box',
-  // },
-  // permissionsHeadGroupCell: {
-  //   fontSize: '16px',
-  //   fontWeight: 'bold',
-  //   textAlign: 'center',
-  //   border: 'none',
-  // },
-  // permissionsTableRow: {
-  //   height: '150px',
-  //   boxSizing: 'border-box',
-  // },
+  permissionsTableBreadcrumbs: {
+    padding: '0 50px',
+    paddingTop: '60px',
+  },
+  permissionsTableLink: {
+    color: '#8cb7e6',
+    textDecoration: 'none',
+  },
+  permissionsTableLinkActive: {
+    color: '#242424',
+    cursor: 'default',
+    textDecoration: 'none',
+  },
+  permissionsTableContainer: {
+    padding: '0 40px',
+    paddingBottom: '60px',
+    boxSizing: 'border-box',
+  },
+  permissionsHeadGroupCell: {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    border: 'none',
+  },
+  permissionsTableRow: {
+    height: '150px',
+    boxSizing: 'border-box',
+  },
 }));
 
 export default useStyles;

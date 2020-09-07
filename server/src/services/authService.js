a19ac9d977e04754f5213b89fb83a534d004cae1
Makc <woohoo1607@gmail.com>
2020-09-07 14:44:45 +0300

    Revert "Merge pull request #194 from BinaryStudioAcademy/fix/admin-page-access"
    This reverts commit bcd7e5d37084485165e8e76f153b4968af84ddef, reversing
    changes made to d0f9e1f9127e37a24346183c9f5ac47c87f04808.
    
    # Conflicts:
    #	server/src/services/authService.js

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import createError from 'http-errors';
 import UserRepository from '../repositories/userRepository';
 import { createToken } from '../helpers/tokenHelper';
@@ -38,44 +37,3 @@ export const autoLogin = async (id, token) => {
     user: { id, email, firstName, lastName },
   };
 };
-=======
-import createError from 'http-errors';
-import UserRepository from '../repositories/userRepository';
-import { createToken } from '../helpers/tokenHelper';
-import { encryptSync } from '../helpers/cryptoHelper';
-
-export const login = async (data) => {
-  const currentUser = await UserRepository.getUserById(data.id);
-  if (!currentUser.isActive) {
-    throw createError(403, 'User account deactivated');
-  }
-  const { id, email, firstName, lastName } = currentUser;
-  await UserRepository.updateById({ id }, { lastLogin: new Date(Date.now()) });
-  return {
-    token: createToken({ id }),
-    user: { id, email, firstName, lastName },
-  };
-};
-
-export const register = async (user) => {
-  const candidate = await UserRepository.getByEmail(user.email);
-  if (!candidate) {
-    await UserRepository.createUsersWithDefaultGroups({
-      ...user,
-      password: encryptSync(user.password),
-    });
-    return {
-      status: 'Register success',
-    };
-  }
-};
-
-export const autoLogin = async (id, token) => {
-  const candidate = await UserRepository.getById({ id });
-  const { firstName, lastName, email } = candidate;
-  return {
-    token: token.split(' ')[1],
-    user: { id, email, firstName, lastName },
-  };
-};
->>>>>>> parent of bcd7e5d... Merge pull request #194 from BinaryStudioAcademy/fix/admin-page-access

c72a06e6ab96b29dd68ba3029e4b8d958794733b
tarkus2000ua <tarkus@i.ua>
2020-09-09 11:54:53 +0300

    Update denyAccesForNonAdminsMiddleware.js

--- server/src/middlewares/denyAccesForNonAdminsMiddleware.js
+++ server/src/middlewares/denyAccesForNonAdminsMiddleware.js
@@ -20,5 +20,25 @@ export const secureUserUpdate = async (req, res, next) => {
     const error = createError(409, 'You can not update user');
     return next(error);
   }
+
+  const mailPattern = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
+  if (req.body.password && req.user.id !== req.params.id) {
+    throw createError(403, "Change of other user's password is prohibited");
+  }
+  if (req.body.id) {
+    throw createError(403, 'Change of user id is prohibited');
+  }
+  if (req.body.lastLogin) {
+    throw createError(403, 'Change of user last login date is prohibited');
+  }
+  if (req.body.firstName === '') {
+    throw createError(400, 'First name cannot be empty');
+  }
+  if (req.body.lastName === '') {
+    throw createError(400, 'Last name cannot be empty');
+  }
+  if (!mailPattern.test(req.body.email)) {
+    throw createError(400, 'Email is invalid');
+  }
   return next();
 };

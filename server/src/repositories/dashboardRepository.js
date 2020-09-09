033cab27d4c34be3e2e0535e270775b2d3767f67
Nikita Remeslov <creatorbon@gmail.com>
2020-09-09 04:32:32 +0300

    Added new methods to repositories

--- server/src/repositories/dashboardRepository.js
+++ server/src/repositories/dashboardRepository.js
@@ -1,5 +1,7 @@
+import { Op } from 'sequelize';
 import models from '../models/index';
 import BaseRepository from './baseRepository';
+import { ACCESS_GRANTED, ACCESS_LIMITED, PRIVATE_COLLECTIONS } from '../config/types';
 
 class DashboardRepository extends BaseRepository {
   getWithVisualizations(id) {
@@ -27,6 +29,51 @@ class DashboardRepository extends BaseRepository {
     return this.model.findAll({
       where: { collections_id: null },
     });
+  }
+
+  getDashboardAccess(id, userId) {
+    return this.model
+      .findOne({
+        attributes: [],
+        where: { id },
+        include: [
+          {
+            model: models.Collection,
+            attributes: ['id'],
+            include: [
+              {
+                model: models.PermissionCollections,
+                attributes: ['permissionGranted'],
+                where: {
+                  [Op.or]: [{ permissionGranted: ACCESS_GRANTED }, { permissionGranted: ACCESS_LIMITED }],
+                },
+                include: [
+                  {
+                    model: models.UserGroups,
+                    as: 'userGroups',
+                    attributes: ['id'],
+                    include: [
+                      {
+                        model: models.User,
+                        where: { id: userId },
+                        attributes: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      })
+      .then((result) => {
+        if (result.Collection !== null) {
+          return result.Collection.PermissionCollections.map((accessType) => {
+            return accessType.userGroups !== null ? accessType.permissionGranted : null;
+          });
+        }
+        return PRIVATE_COLLECTIONS;
+      });
   }
 }
 

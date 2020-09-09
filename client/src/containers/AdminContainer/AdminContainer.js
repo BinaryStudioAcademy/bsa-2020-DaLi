711bcae9107cd12a9b9720a9a9a65b414ad282aa
Nikita Remeslov <creatorbon@gmail.com>
2020-09-09 22:27:28 +0300

    Removed route databases/:id

--- client/src/containers/AdminContainer/AdminContainer.js
+++ client/src/containers/AdminContainer/AdminContainer.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import PeoplePageContainer from '../PeoplePageContainer/PeoplePageContainer';
 import PermissionsContainer from '../PermissionsContainer/PermissionsContainer';
 import DatabasesPageContainer from '../DatabasesPageContainer/DatabasesPageContainer';
-// import { ConnectionDatabasePage } from '../../pages/ConnectionDatabasePage';
 import ConnectionDatabaseContainer from '../ConnectionDatabaseContainer/ConnectionDatabaseContainer';
 
 function AdminContainer() {
@@ -14,13 +13,6 @@ function AdminContainer() {
         <Route exact path="/admin/permissions/:id" component={PermissionsContainer} />
         <Route path="/admin/people" component={PeoplePageContainer} />
         <Route exact path="/admin/databases" component={DatabasesPageContainer} />
-        <Route
-          exact
-          path="/admin/databases/:id"
-          component={({ match }) => {
-            return <div>Database id {match.params.id}</div>;
-          }}
-        />
         <Route path="/admin/connection-database" component={ConnectionDatabaseContainer} />
       </Switch>
     </main>
